e1ee2c5e971dd59249faef517ac7b8586eea20b5
Wenjun Li <simon30302@hotmail.com>
2020-02-05 01:28:40 +0000

    split the parseInput() into a seprate file

---
+++ loading.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def parseInputs(fname):
+    filename = "F:\\PG CS Study\\IntroToML\\cw\\decision_trees_65\\data\\" + fname
+    # filename = os.getcwd() + '//data//' + fname
+    f = open (filename, 'r')
+    line = f.readline()
+    numArray = []
+    charArray =[]
+    while line:
+        tempNumList = []
+        result = [x.strip() for x in line.split(',')]
+        for str in result:
+            if(str.isalpha() == True):
+                charArray.append(str)
+            if (str.isdigit() == True):
+                tempNumList.append(int(str))
+        line = f.readline()
+        numArray.append(tempNumList)
+    f.close()
+    # https://stackoverflow.com/questions/10346336/list-of-lists-into-numpy-array?rq=1
+    numpyNumArray = np.array([np.asarray(xi) for xi in numArray])
+    numpyCharArray = np.array(charArray)
+    # print(numpyNumArray)
+    # print(numpyCharArray)
+
+    return numpyNumArray, numpyCharArray
